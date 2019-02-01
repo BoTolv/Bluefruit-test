28222e39f6c257e3949434e107f64c024c621be3
BoTolv <47107472+BoTolv@users.noreply.github.com>
2019-02-01 13:24:00 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -106,7 +106,8 @@ function onSend(){
 }
 
 function disconnect() {
-	ble.disconnect//(deviceId, onDisconnect, onError);
+	ble.disconnect(deviceId, onDisconnect, onError);
+	document.getElementById("statusDiv").innerHTML = "Status: Disconnected";
 }
 
 function onDisconnect(){
